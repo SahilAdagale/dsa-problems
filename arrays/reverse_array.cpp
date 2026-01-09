b3fe66861d80cbe61ac44bcb4afafa9707617945
Sahilada <sahiladagale16@gmail.com>
2026-01-09 20:30:39 +0530

    feat: add reverse array implementation

---
+++ arrays/reverse_array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void printarr(int* arr,int n){
+    for(int i=0;i<n;i++){
+        cout<< arr[i]<< " "; 
+    }
+    cout << endl;
+}
+
+int main(){
+    int arr[]={2,4,3,6,4,7};
+    int n=sizeof(arr)/sizeof(int);
+
+    int start=0;
+    int end=n-1;
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+
+    }
+    printarr(arr,n);
+    return 0;
+}
