8e55d9daf880f3effb411d2fb01dc0a33b7d6f33
Sahilada <sahiladagale16@gmail.com>
2026-01-24 00:22:00 +0530

    Add single element in sorted array implementation

---
+++ arrays/single_element_in_sorted_array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int  sortedArray(int* arr,int n){
+    if(n==1) return arr[0];
+    if(arr[0] != arr[1]) return arr[0];
+    if(arr[n-1] != arr[n-2]) return arr[n-1];
+
+    int low=1, high=n-2;
+    while(low<=high){
+        int mid= (low+high)/2;
+        if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) return arr[mid];
+
+        if(mid % 2 == 1 && arr[mid] == arr[mid-1] || mid % 2 == 0 && arr[mid] == arr[mid+1]){
+            low=mid+1;
+        }else{
+            high=mid-1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[]={1,1,3,3,4,4,5,5,6,6,1,8,8};
+    int n=sizeof(arr)/sizeof(int);
+
+  cout << "The single element in sorted array is: "<<  sortedArray(arr,n) <<endl;
+}
