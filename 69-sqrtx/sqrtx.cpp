a3083eb359a72749356abf94d149fb4657bf1e45
Sahil Adagale <sahiladagale16@gmail.com>
2026-03-16 21:19:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (49.98%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     int mySqrt(int x) {
 
-        int ans=0;
+        int left=1, right=x , ans=0;
 
-        for(long long i=1; i*i <= x; i++){
-            ans=i;
+        while(left <= right){
+            long long mid= left + (right - left) / 2;
+
+            if(mid <= x / mid){
+                ans= mid;
+                left = mid +1;
+            }
+            else{
+                right = mid-1;
+            }
         }
         return ans;
-        
-        
     }
 };
